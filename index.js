00c366e8097b1e300e28291393b432fefb635b35
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-03-26 23:42:23 +0200

    Commence configuration storage

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 /* Frankenstein -- Automated file splitter/recombiner. */
+
+const fs = require('fs');
+const path = require('path');
+
+/* Configuration / data file */
+class Configuration {
+
+  constructor(path) {
+    this.path = path;
+    this.data = null;
+  }
+
+  create() {
+    this.data = {dirs: path.resolve(this.path), blocksize: '16M'};
+  }
+
+  load() {
+    const rawData = fs.readFileSync(this.path, 'utf-8');
+    const data = JSON.parse(rawData);
+    this.data = data;
+  }
+
+  save() {
+    fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
+  }
+
+}
